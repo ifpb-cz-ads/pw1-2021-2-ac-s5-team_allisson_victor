08e3714712d3ecc376a3020231464c5ad73ce69c
VictorBrasil <48683006+VictorBrasil@users.noreply.github.com>
2021-11-29 21:11:11 -0300

    Adicionando questões pares

---
+++ Atividade 5.02/Questão 2.py
@@ -0,0 +1,7 @@
+Distancia = int(input("Informe a distancia a percorrer:"))
+if Distancia > 200:
+    valor = Distancia*0.45
+else:
+    valor = Distancia*0.50
+
+print("Valor total será:",valor)    
